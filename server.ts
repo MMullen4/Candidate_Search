cfe7dd86805c45b15bce1800232c7d9b24a0241e
Matt Mullen <mhmullen@icloud.com>
2025-04-07 15:15:04 -0700

    revised vite env

--- server.ts
+++ server.ts
@@ -6,14 +6,17 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 
 // Serve static files from the dist directory
-app.use(express.static(path.join(__dirname, "dist")));
+// app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static('dist'));
 
 // Handle all routes by serving the index.html
 app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "dist", "index.html"));
+    // res.sendFile(path.join(__dirname, "dist", "index.html"));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const port = process.env.PORT || 3000;
-app.listen(Number(port), "0.0.0.0", () => {
+// app.listen(Number(port), "0.0.0.0", () => {
+    app.listen(Number(port), () => {
     console.log(`Server is running on port ${port}`);
 });
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  envDir: './environment',
+  envDir: './env',
   plugins: [react()],
   server: {
     port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
